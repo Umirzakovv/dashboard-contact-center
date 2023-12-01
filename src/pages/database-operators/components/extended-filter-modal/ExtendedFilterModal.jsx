b676e98fb9fa615f88c15ace7f33ecd813e6c518
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2023-12-01 10:24:51 +0500

    create layout of select component

--- src/pages/database-operators/components/extended-filter-modal/ExtendedFilterModal.jsx
+++ src/pages/database-operators/components/extended-filter-modal/ExtendedFilterModal.jsx
@@ -1,7 +1,25 @@
+import CancelBtn from "../../../../components/cancel-btn/CancelBtn";
+import SubmitBtn from "../../../../components/submit-btn/SubmitBtn";
+import Select from "../select/Select";
 import "./extended-filter-modal.scss";
 
 const ExtendedFilterModal = () => {
-  return <div className="extended-filter-modal">ExtendedFilterModal</div>;
+  return (
+    <div className="extended-filter-modal">
+      <div className="extended-filter__selects">
+        <Select />
+        <Select />
+        <Select />
+        <Select />
+        <Select />
+        <Select />
+      </div>
+      <div className="extended-filter__btns">
+        <CancelBtn title="Сбросить"/>
+        <SubmitBtn title="Фильтрировать"/>
+      </div>
+    </div>
+  );
 };
 
 export default ExtendedFilterModal;
---
+++ src/pages/database-operators/components/select/Select.jsx
@@ -0,0 +1,18 @@
+import "./select.scss";
+
+const Select = () => {
+  return (
+    <div className="extended-filter__select">
+      <p className="extended-filter__select-title">Стаж</p>
+      <select>
+        <option value="123">123</option>
+        <option value="123">123</option>
+        <option value="123">123</option>
+        <option value="123">123</option>
+        <option value="123">123</option>
+      </select>
+    </div>
+  );
+};
+
+export default Select;
