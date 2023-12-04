638dad0af4801570fced32f626ba545af0efabb1
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2023-12-04 10:58:45 +0500

    create layout of more and edit btns that have blue color

--- src/components/edit-btn-blue/EditBtnBlue.jsx
+++ src/components/edit-btn-blue/EditBtnBlue.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/prop-types */
 import editBtn from "../../assets/icons/pencil-blue.svg";
 import "./edit-btn-blue.scss";
-const EditBtnBlue = () => {
+
+const EditBtnBlue = ({ onClick }) => {
   return (
-    <button className="edit-btn__blue">
+    <button className="edit-btn__blue" onClick={onClick}>
       <img src={editBtn} alt="edit button" />
     </button>
   );
--- src/pages/database-operators/components/more-btn/MoreBtn.jsx
+++ src/pages/database-operators/components/more-btn/MoreBtn.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 import "./more-btn.scss";
 import moreBtn from "../../../../assets/icons/eye-blue.svg";
 
-const MoreBtnBlue = () => {
+const MoreBtnBlue = ({ onClick }) => {
   return (
-    <button className="more-btn__blue">
+    <button className="more-btn__blue" onClick={onClick}>
       <img src={moreBtn} alt="more button" />
     </button>
   );
