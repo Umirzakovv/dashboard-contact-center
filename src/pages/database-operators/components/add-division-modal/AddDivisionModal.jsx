bfe2e5f9d616dfc7050c0d671c0b54ada2e71b1c
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2023-12-06 17:30:18 +0500

    create modal of add division component and link it to api

--- src/pages/database-operators/components/add-division-modal/AddDivisionModal.jsx
+++ src/pages/database-operators/components/add-division-modal/AddDivisionModal.jsx
@@ -3,8 +3,12 @@ import { useEffect } from "react";
 import "./add-division-modal.scss";
 import { useRef } from "react";
 import SubmitBtn from "../../../../components/submit-btn/SubmitBtn";
+import { useState } from "react";
 
 const AddDivisionModal = ({ setIsModalOpen }) => {
+  const [inputValue, setInputValue] = useState();
+  const [error, setError] = useState();
+
   const modalRef = useRef();
   useEffect(() => {
     let handler = (e) => {
@@ -18,13 +22,43 @@ const AddDivisionModal = ({ setIsModalOpen }) => {
       document.removeEventListener("mousedown", handler);
     };
   });
+
+  const handleFormChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://192.168.104.70:2004/api/v1/division/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: inputValue,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        console.log(inputValue);
+      })
+      .catch((error) => setError(error));
+    setIsModalOpen(false);
+  };
+
   return (
-    <form className="add-division__modal" ref={modalRef}>
+    <form
+      className="add-division__modal"
+      ref={modalRef}
+      onSubmit={handleFormSubmit}
+    >
       <h2 className="add-division__title">Добавить новый список сотрудников</h2>
       <input
         type="text"
         className="add-division__input"
         placeholder="Напишите..."
+        onChange={handleFormChange}
       />
       <SubmitBtn title="Добавить" />
     </form>
