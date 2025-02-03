fc3c97b6492325f066b2c67abdf1f343d53c15d2
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2025-02-03 14:08:15 +0500

    Create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Services from "./pages/services/Services";
 import LateComers from "./pages/latecomers/LateComers";
+import Login from "./pages/login/Login";
+import { useEffect } from "react";
 
 const App = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("dashboard_token");
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    } else {
+      navigate("/");
+    }
+  }, [navigate, token]);
+
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Dashboard />} />
       <Route path="/services" element={<Services />} />
       <Route path="/latecomers" element={<LateComers />} />
